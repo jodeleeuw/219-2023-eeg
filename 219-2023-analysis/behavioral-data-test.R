ab12daaa564dbb71b658679653767306fecff14c
Josh de Leeuw <josh.deleeuw@gmail.com>
2023-02-05 12:48:48 -0500

    testing the behavioral data files

---
+++ 219-2023-analysis/behavioral-data-test.R
@@ -0,0 +1,6 @@
+library(jsonlite)
+library(dplyr)
+
+df <- fromJSON('data/behavior/219_2023_behavioral_88.json')
+
+card_reveals <- df %>% filter(task=="reveal") %>% select(eeg_event_id, card, color, participant_choice, card_value, wins_so_far, outcome, sequence_id)
